22351f16cb0dbc827b3339c7922d1b941ea1b9a0
Your Name <zhangyinyuan@aliyun.com>
2018-05-15 18:40:28 +0800

    angular done

---
+++ angular-start/js/scope.js
@@ -0,0 +1,50 @@
+var myapp = angular.module("myapp", []);
+
+// Scope(作用域) 是应用在 HTML (视图) 和 JavaScript (控制器)之间的纽带。
+// Scope 是一个对象，有可用的方法和属性。
+// Scope 可应用在视图和控制器上。
+// 依赖注入:依赖'$scope',注入到$scope 
+// myapp.controller('MainCtrl', ['$scope', function($scope){
+// 	$scope.username = "汤姆熊";
+// 	this.username = "mainCtrl-username";
+
+// 	$scope.append = function(){
+// 		$scope.username += "append ";
+// 	};
+// }])
+myapp.controller('MainCtrl', function($scope, $http, $location, $timeout, $interval) {
+	$scope.username = "汤姆熊";
+	this.username = "mainCtrl-username";
+	$scope.append = function() {
+		$scope.username += "append ";
+	};
+
+	$scope.url = $location.absUrl();
+
+	// http get 函数
+	//	$http.get('https://www.apiopen.top/meituApi?page=1').then(function successCallback(response) {
+	//		console.log(response);
+	//	}, function errorCallback(response) {
+	//		console.log(response);
+	//	});
+
+	$http({
+		method: 'GET',
+		url: 'https://www.apiopen.top/meituApi1?page=1'
+	}).then(function successCallback(response) {
+		console.log(response.data.msg);
+	}, function errorCallback(response) {
+		console.log(response);
+	});
+
+	//超时执行函数
+	//	$timeout(function() {
+	//		alert("first")
+	//	}, 2000);
+
+	//周期性执行函数
+	$interval(function() {
+		$scope.nowTime = new Date().toLocaleTimeString();
+	}, 1000);
+
+});
---
+++ data/subline-使用.md
@@ -0,0 +1,7 @@
+
+Sublime 设置快捷键让html在浏览器中打开
+https://blog.csdn.net/chy555chy/article/details/54962679
+
+sublime text3 如何快速启动自定义html模板？
+https://segmentfault.com/q/1010000005184945/a-1020000005185092
+
